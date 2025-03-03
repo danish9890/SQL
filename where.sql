af56ebcde57e14850d91ea9a6685dc4e81ac612b
Danish Nadaf <137716814+danish9890@users.noreply.github.com>
2025-03-03 23:31:58 +0530

    Update where.sql

--- where.sql
+++ where.sql
@@ -1,5 +1,5 @@
 select *
 from parks_and_recreation.employee_demographics
 #where gender !="female" and birth_date > "1985-03-05" and age > 35 or not gender = "male";
-#where (first_name="tom" and age= 36) or age > 55
+where (first_name="tom" and age= 36) or age > 55
 where first_name like "a___%";
