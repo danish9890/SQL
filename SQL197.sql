3fb5005955f43acdd510c971293bbd110eec4aed
Danish Nadaf <137716814+danish9890@users.noreply.github.com>
2025-05-15 23:28:33 +0530

    Update SQL197.sql

--- SQL197.sql
+++ SQL197.sql
@@ -1,1 +1,28 @@
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+
 
