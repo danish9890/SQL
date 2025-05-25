fa5a9b3c96f8788f37ad0cdb14679dc696fd8c81
Danish Nadaf <137716814+danish9890@users.noreply.github.com>
2025-05-25 22:02:34 +0530

    Update SQL1211.sql

--- SQL1211.sql
+++ SQL1211.sql
@@ -59,3 +59,8 @@ Dog queries poor_ query_percentage is (1 / 3) * 100 = 33.33
 
 Cat queries quality equals ((2 / 5) + (3 / 3) + (4 / 7)) / 3 = 0.66
 Cat queries poor_ query_percentage is (1 / 3) * 100 = 33.33
+
+
+ans==
+select query_name , round(avg(rating/position),2) as quality , round(avg(if(rating <3 , 1,0))*100,2) as poor_query_percentage from Queries
+group by query_name ;
