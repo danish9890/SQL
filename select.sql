f49742fd05b422aa8522d735d1af54c2dc7130e7
Danish Nadaf <137716814+danish9890@users.noreply.github.com>
2025-06-01 23:42:27 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -5,4 +5,4 @@ age*10
 from parks_and_recreation.employee_demographics;
 
 select distinct first_name, gender
-from parks_and_recreation.employee_demographics
+from parks_and_recreation.employee_demographics;
